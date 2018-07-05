27969fe2a31940c24a32ce06d3fb740ff329b7c7
LuxChen <csm1398@yahoo.com>
2018-07-05 17:52:18 +0800

    add support for devtool and properties

--- src/main/java/hello/Example.java
+++ src/main/java/hello/Example.java
@@ -1,21 +1,11 @@
 package hello;
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(properties.AcmeProperties.class)
 public class Example {
-
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
-	}
-
-	@RequestMapping("/fetchName")
-	String fetchName() {
-		return "my name is lux.chen nice to meet you";
-	}
-
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Example.class, args);
 	}
--- src/main/java/hello/User.java
+++ src/main/java/hello/User.java
@@ -14,6 +14,8 @@ public class User {
     private String name;
 
     private String email;
+    
+    private String telnumber;
 
 	public Integer getId() {
 		return id;
@@ -37,6 +39,14 @@ public class User {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public String getTelnumber() {
+		return telnumber;
+	}
+
+	public void setTelnumber(String telnumber) {
+		this.telnumber = telnumber;
 	}
 
 
